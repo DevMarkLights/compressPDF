4a6dd815cf5c73f09b33fc69d7fbb941c7c3f67e
Mark Lights <marklights899@gmail.com>
2026-02-27 14:42:14 -0500

    change port to match port increments on pi

--- runApp.bash
+++ runApp.bash
@@ -4,4 +4,4 @@ cd frontend
 cd ../
 cd backend
 source .venv/bin/activate
-python -m uvicorn main:app --host 0.0.0.0 --port 8084 --workers 1
+python -m uvicorn main:app --host 0.0.0.0 --port 8083 --workers 1
