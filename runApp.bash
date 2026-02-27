a155536181a7d3e7e5f8192ea1b07fb575e42bc4
Mark Lights <marklights899@gmail.com>
2026-02-27 00:53:57 -0500

    .

--- runApp.bash
+++ runApp.bash
@@ -3,7 +3,5 @@ cd frontend
 
 cd ../
 cd backend
-# CMD ['uvicorn', 'main:app', '--host','0.0.0.0','--port','8084','--workers', '1']
 source .venv/bin/activate
-# .venv/bin/uvicorn main:app --host 0.0.0.0 --port 8084 --workers 1
 python -m uvicorn main:app --host 0.0.0.0 --port 8084 --workers 1
